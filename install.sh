4d84fa422baeb906405f74a9cf0aaec3e2eeaf53
diogomsantos <diogomsantos@hotmail.com>
2016-03-27 07:45:55 +0000

    Remove Kweb

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
+./helpers/ginstall-ytdl
 sudo -s <<EOF
 apt-get install omxplayer -y
-EOF
-wget http://steinerdatenbank.de/software/kweb-1.7.0.tar.gz
-tar -xzf kweb-1.7.0.tar.gz
-cd kweb-1.7.0
-yes | ./debinstall
-sudo -s <<EOF
-cd ..
 if [ ! -d /etc/chromium-browser ]; then
   chmod +x install-chromium.sh
   ./install-chromium.sh
 fi
+cp helpers/* /usr/local/bin
 cp install-chromium.sh /usr/bin/
 cp native/run_omxplayer.py /usr/bin/run_omxplayer.py
 mkdir /etc/chromium-browser/native-messaging-hosts
